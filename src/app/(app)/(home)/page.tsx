dc86d9061e723ad7c53a8110d703eb5ecd90ed0f
pwilliams-ck <pwilliams@cloudkey.io>
2025-04-26 18:44:13 -0500

    payload-02: add categories collection

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,35 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 
-export default function Home() {
-  return (
-    <div className="p-4">
-      <div className="flex flex-col gap-y-4">
-        <div>
-          <Button variant="default">This is a button</Button>
-        </div>
-        <div>
-          <Button variant="elevated">This is a button</Button>
-        </div>
-        <div>
-          <Button variant="secondary">This is a button</Button>
-        </div>
-        <div>
-          <Input placeholder="I am an input" />
-        </div>
-        <div>
-          <Progress value={50} />
-        </div>
-        <div>
-          <Textarea value="I am a textarea" />
-        </div>
-        <div>
-          <Checkbox />
-        </div>
-      </div>
-    </div>
-  );
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "categories",
+  });
+
+  return <div className="p-4">{JSON.stringify(data, null, 2)}</div>;
 }
